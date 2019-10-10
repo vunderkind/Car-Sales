6f49b2815e2f90ce92b0337daddf675847bb13f9
vunderkind <justin.irabor@gmail.com>
2019-10-10 08:55:42 +0100

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //import combineReducers and createStore to make the redux store 
 import {combineReducers, createStore} from 'redux';
+import {carReducer} from './reducer/reducer';
+import {Provider} from 'react-redux';
 
 //import reducers 
 import * as reducers from './reducer/reducer';
@@ -10,8 +12,12 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const godzillaReducer = combineReducers({state:reducers.carReducer});
-const store = createStore();
+//Attempted to use a monster reducer, but PCR showed me this was unnecessarily
+// const godzillaReducer = combineReducers({state:reducers.carReducer});
+const store = createStore(carReducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, rootElement);
